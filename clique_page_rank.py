a723259b8c4ce3abe77ec107be24002e7d5b675e
Anirudh <anirudh95@gmail.com>
2020-12-23 03:55:07 +0000

    Added clique page rank

---
+++ clique_page_rank.py
@@ -0,0 +1,50 @@
+from graphframes import *
+from functools import reduce
+from pyspark import *
+from pyspark.sql import *
+from operator import add
+import shortuuid
+
+spark = SparkSession.builder.appName('fun').getOrCreate() 
+
+from pyspark.sql.types import Row
+import uuid
+from pyspark.sql.functions import monotonically_increasing_id
+from uuid import UUID
+
+def parse_vert(x, delimit):
+    hyper_return =  x.value.split(delimit)
+    try:
+        hyper_return.remove("")
+    except:
+        pass
+    for v in hyper_return:
+        yield v,"vertex"
+
+def parse_edge(x, delimit):
+    hyper_return =  x.value.split(delimit)
+    try:
+        hyper_return.remove("")
+    except:
+        pass
+    for i in range(len(hyper_return)):
+        for j in range(i+1,len(hyper_return)):
+            yield hyper_return[i],hyper_return[j]
+            yield hyper_return[j],hyper_return[i]
+
+def init_graph():
+    vertices = spark.read.text("email-enron.txt").rdd.flatMap(lambda x: parse_vert(x, " "))#.toDF(["id","type"])
+    edges = spark.read.text("email-enron.txt").rdd.flatMap(lambda x: parse_edge(x, " "))#.toDF(["id","type"])
+    df_vert = vertices.map(lambda x:(x[0],x[1])).toDF(["id","type"]).dropDuplicates()
+    df_edge = edges.map(lambda x:(x[0],x[1])).toDF(["src","dst"]).dropDuplicates()
+    print("vertices collect",vertices.collect())
+    print("edges collect", edges.collect()) 
+    df_vert.show()
+    df_edge.show()
+    new_g = GraphFrame(df_vert, df_edge)
+    return new_g
+
+new_g = init_graph()
+results2 = new_g.pageRank(resetProbability=0.15, maxIter=1)
+print(results2.vertices.show())
+
